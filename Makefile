c44dc52d704fc12927a61a9b8aa54443278c9108
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2014-11-10 18:27:14 +0100

    modified:   Makefile new file:   extra/mac/engopts.sh modified:   mscript.sh - Mac branch.

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 SHELL=/bin/bash
-MATLAB_ROOT=/usr/local/MATLAB/R2014a
-ARCH=glnxa64
+
+## for Linux
+# MATLAB_ROOT=/usr/local/MATLAB/R2014a
+# ARCH=glnxa64
+
+## for Mac
+MATLAB_ROOT=/Applications/MATLAB_R2014a.app
+ARCH=maci64
 
 
 all:
--- mscript.sh
+++ mscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Don't forget the: /bin/csh is required 
 #  sudo aptitude install csh
-MATLAB_ROOT=/usr/local/MATLAB/R2014a
-ARCH=glnxa64
+MATLAB_ROOT="/Applications/MATLAB_R2014a.app"
+ARCH=maci64
 export LD_LIBRARY_PATH=$MATLAB_ROOT/bin/$ARCH:$MATLAB_ROOT/sys/os/$ARCH:$LD_LIBRARY_PATH
 
 MSCRIPT=$(readlink -f $0 | sed 's/\.sh$//')
